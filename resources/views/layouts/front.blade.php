167a2a392e7ace6dbf5791cdc059790a4e62c140
Md. Yasir Aarafat <39982576+yasirarafat28@users.noreply.github.com>
2020-11-09 23:51:19 +0600

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -38,11 +38,11 @@
         <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
             <div class="container">
                 <div class="row align-items-center">
-                    <div class="col-6 col-xl-6">
+                    <div class="col-6 col-xl-5">
                         <h1 class="mb-0 site-logo"><a href="index-2.html" class="h2 mb-0">{{\App\Setting::setting()->app_name}}<span
                                     class="text-primary">.</span> </a></h1>
                     </div>
-                    <div class="col-12 col-md-6 d-none d-xl-block">
+                    <div class="col-12 col-md-7 d-none d-xl-block">
                         <nav class="site-navigation position-relative text-right" role="navigation">
                             <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
                                 <li><a href="#home-section" class="nav-link">Home</a></li>
