e6f45db3085f45ba4c15839f9ba182b82f3fe4cf
Md. Kawsar Ali <kawsar4585@gmail.com>
2020-11-10 10:35:08 +0600

    Update member-find.blade.php

--- resources/views/admin/member-find.blade.php
+++ resources/views/admin/member-find.blade.php
@@ -11,26 +11,17 @@
 
 <!-- Main Content -->
 <section class="content">
-    <div class="block-header">
-        <div class="row">
-            <div class="col-lg-7 col-md-6 col-sm-12">
-            </div>
-            <div class="col-lg-5 col-md-6 col-sm-12">
-                <ul class="breadcrumb float-md-right">
-                    <li class="breadcrumb-item"><a href="#"><i class="zmdi zmdi-home"></i> {{\App\Setting::setting()->app_name}}</a></li>
-                    <li class="breadcrumb-item"><a href="javascript:void(0);">সদস্য খুঁজুন</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
+
     <div class="container-fluid">
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">সদস্য খুঁজুন</h1>
 
+            <ul class="breadcrumb float-md-right">
+                <li class="breadcrumb-item"><a href="#"><i class="zmdi zmdi-home"></i> {{\App\Setting::setting()->app_name}}</a></li>
+                <li class="breadcrumb-item"><a href="javascript:void(0);">সদস্য খুঁজুন</a></li>
+            </ul>
+        </div>
         <div class="row clearfix">
-
-
-
-
-
 
 
             <div class="col-sm-12 col-md-12 col-lg-12">
