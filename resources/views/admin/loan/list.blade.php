08cce64855e76df647617c24387651c0e0f6e197
Md. Yasir Aarafat <39982576+yasirarafat28@users.noreply.github.com>
2020-12-12 23:32:32 +0600

    Update list.blade.php

--- resources/views/admin/loan/list.blade.php
+++ resources/views/admin/loan/list.blade.php
@@ -126,14 +126,6 @@
                                     <a href="?limit=-1" class="btn btn-success">সবগুলো দেখুন</a>
                                 </div>
 
-                                <div class="col-lg-2 col-md-2 text-right">
-                                    <button type="button" class="btn btn-neutral hidden-sm-down" onclick="$('.buttons-csv')[0].click();">
-                                        <i class="zmdi zmdi-archive"></i>
-                                    </button>
-                                    <button type="button" class="btn btn-neutral hidden-sm-down" onclick="$('.buttons-print')[0].click();">
-                                        <i class="zmdi zmdi-print"></i>
-                                    </button>
-                                </div>
                             </div>
                         </form>
 
