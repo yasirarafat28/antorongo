61c9445d49bbead12ab3b1145b94250ce3a61464
Md. Kawsar Ali <kawsar4585@gmail.com>
2021-02-15 11:21:48 +0600

    Update list.blade.php

--- resources/views/admin/saving/list.blade.php
+++ resources/views/admin/saving/list.blade.php
@@ -77,7 +77,7 @@
                                 <span style="font-size: 15px;">মোট লাভ</span>
                                 <div class="h6 mb-0 font-weight-bold text-gray-800">৳ {{App\NumberConverter::en2bn($closed_total_profit_paid)}} টাকা</div>
                                 <span style="font-size: 15px;">মোট ফেরত </span>
-                                {{-- <div class="h6 mb-0 font-weight-bold text-gray-800">৳ {{App\NumberConverter::en2bn($closed_saving_profit)}} টাকা</div> --}}
+                                <div class="h6 mb-0 font-weight-bold text-gray-800">৳ {{App\NumberConverter::en2bn($closed_target_amount + $closed_total_profit_paid)}} টাকা</div>
                             @endif
                                 <a href="/admin/saving/{{$type}}/list?filterBy=closed" class="text-link">তালিকা দেখুন</a>
                             </div>
