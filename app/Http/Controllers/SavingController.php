67b2809fe5add6835af4a02342ab55e8ef712bb5
Md. Kawsar Ali <kawsar4585@gmail.com>
2021-02-15 11:17:28 +0600

    fixes

--- app/Http/Controllers/SavingController.php
+++ app/Http/Controllers/SavingController.php
@@ -879,7 +879,7 @@ class SavingController extends Controller
         // $closed_saving_profit   = $closed_target_amount + $closed_saving_return_amount ;
 
         return view('admin/saving/list',compact('records','type','active_count','pending_count','declined_count',
-        'closed_count','closed_target_amount','closed_saving_return_amount','closed_saving_profit','closed_total_profit_paid','closed_total_revenue_paid'));
+        'closed_count','closed_target_amount','closed_total_profit_paid','closed_total_revenue_paid'));
     }
 
     public function getSavingsByUser(Request $request)
