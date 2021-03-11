c6acb804331f77dfdbbe22c63d93a56898405884
Md. Kawsar Ali <kawsar4585@gmail.com>
2021-03-11 18:05:37 +0600

    Update saving-audit-list.blade.php

--- resources/views/admin/audit/saving-audit-list.blade.php
+++ resources/views/admin/audit/saving-audit-list.blade.php
@@ -182,7 +182,7 @@
 
                                 @foreach($records ?? array() as $item)
                                     <tr>
-                                        {{$i++}}
+                                        <td>{{$i++}}</td>
                                         <td>{{$item->user->name_bn??'N/A'}}</td>
                                         <td>{{$item->user->unique_id??'N/A'}}</td>
                                         <td>{{\App\Saving::get_total_diposit_in_range($item->id,$from,$to)??'N/A'}}</td>
