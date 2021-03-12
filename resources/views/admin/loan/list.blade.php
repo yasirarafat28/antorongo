d005d280f98244fdcb198cf00242395c309b9b43
Md. Kawsar Ali <kawsar4585@gmail.com>
2021-03-12 17:07:29 +0600

    Update list.blade.php

--- resources/views/admin/loan/list.blade.php
+++ resources/views/admin/loan/list.blade.php
@@ -258,7 +258,7 @@
                                     <th> মোট পরিশোধ</th>
                                     <th> মোট বকেয়া</th>
                                     <th> তারিখ </th>
-                                    <th> মেয়াদ শেষ </th>
+                                    {{-- <th> মেয়াদ শেষ </th> --}}
                                     <th>  অবস্থা</th>
                                 </tr>
                                 </thead>
@@ -273,7 +273,7 @@
                                     <th> মোট পরিশোধ</th>
                                     <th> মোট বকেয়া</th>
                                     <th> তারিখ </th>
-                                    <th> মেয়াদ শেষ </th>
+                                    {{-- <th> মেয়াদ শেষ </th> --}}
                                     <th>  অবস্থা</th>
                                 </tr>
                                 </tfoot>
@@ -319,7 +319,7 @@
                                         <td>{{\App\NumberConverter::en2bn($item->interests->sum('amount')   +   $item->paid_reveanues->sum('amount'))}}</td>
                                         <td>{{\App\NumberConverter::en2bn($item->current_payable())}}</td>
                                         <td>{{date('Y/m/d',strtotime($item->start_at))}}</td>
-                                        <td>{{date('Y/m/d',strtotime($item->closed_at))}}</td>
+                                        {{-- <td>{{date('Y/m/d',strtotime($item->closed_at))}}</td> --}}
                                         <td>{{$item->status}}</td>
 
                                     </tr>
